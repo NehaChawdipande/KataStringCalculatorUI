b7ba44277ee496d773f75b7b1fe97d180c72b830
NehaChawdipande <nehachawdipande10@gmail.com>
2024-09-18 20:39:28 +0530

    added existing kata string logic with minor tweeks to support TS

--- KataStringCalculator/src/app/app.component.spec.ts
+++ KataStringCalculator/src/app/app.component.spec.ts
@@ -3,13 +3,12 @@ import { AppComponent } from './app.component';
 import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
-
-    const fixture = TestBed.createComponent(AppComponent);
-    const app: AppComponent = fixture.componentInstance;
   });
 
   it('should create the app', () => {
@@ -75,4 +74,90 @@ it('should return 0 button is clicked with empty input', () => {
   expect(value).toBe(0);
 });
 
+// it case 2: Single number
+it('should return the same number for a single number', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app: AppComponent = fixture.componentInstance;
+  app.inputValue = '5';
+  const value = app.calculate();
+  expect(value).toBe(5);
+});
+
+// it case 3: Multiple numbers separated by commas
+it('should add multiple numbers separated by commas', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app: AppComponent = fixture.componentInstance;
+
+  app.inputValue = '1,2,3';
+  const value = app.calculate();
+  expect(value).toBe(6);
+});
+
+// it case 4: Numbers separated by newlines
+it('should add numbers separated by newlines', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app: AppComponent = fixture.componentInstance;
+
+  app.inputValue = '1\n2\n3';
+  const value = app.calculate();
+  expect(value).toBe(6);
+});
+
+
+// it case 6: Numbers separated by custom delimiter
+it('should add numbers with a custom delimiter', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app: AppComponent = fixture.componentInstance;
+
+  app.inputValue = "//:\\n1:2:3:4";
+  const value = app.calculate();
+  expect(value).toBe(10);
+});
+
+// it case 7: Negative numbers
+it('should throw an error for negative numbers', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app: AppComponent = fixture.componentInstance;
+
+  app.inputValue = '-1,2,3'
+  expect(() => app.calculate()).toThrowError('Negatives not allowed: -1');
+});
+
+// it case 8: Multiple Negative numbers
+it('should throw an error for multiple negative numbers', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app: AppComponent = fixture.componentInstance;
+
+  app.inputValue = '-1,2,3,-4';
+  
+  expect(() => app.calculate()).toThrowError('Negatives not allowed: -1, -4');
+});
+
+// it case 9: Numbers larger than 1000
+it('should ignore numbers larger than 1000', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app: AppComponent = fixture.componentInstance;
+  app.inputValue = '1001,2,3';
+  const value = app.calculate();
+  expect(value).toBe(5);
+});
+
+// it case 10: Multiple Numbers larger than 1000
+it('should ignore multiple numbers larger than 1000', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app: AppComponent = fixture.componentInstance;
+  app.inputValue = '1001,2,3, 2003';
+  const value = app.calculate();
+  expect(value).toBe(5);
+});
+
+
+// it case 10: custom delimiters of any length
+it('should add numbers with a custom delimiter of any length', () => {
+  const fixture = TestBed.createComponent(AppComponent);
+  const app: AppComponent = fixture.componentInstance;
+  app.inputValue = '//**\\n1**2**3';
+  const value = app.calculate();
+  expect(value).toBe(6);
+});
 });
--- KataStringCalculator/src/app/app.component.ts
+++ KataStringCalculator/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+    FormsModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -18,9 +21,49 @@ export class AppComponent {
   }
 
   calculate(): number {
-    if (this.inputValue === '')
+    if (this.inputValue === '') {
+      this.sum = 0;
+      console.log('input null');
+
       return 0;
-    else
-      return this.sum;
+    }
+    else {
+      const delimiters = [',', '\n'];
+
+      // check for custom delimiters
+      const customDelimiterMatch = this.inputValue.match(/\/\/([^\]]+)\\n/);
+
+      const delimiter = customDelimiterMatch && customDelimiterMatch[1];
+
+      if (delimiter)
+        delimiters.push(delimiter); // push the delimiter to the array if found.
+
+      const delimiterRegex = new RegExp(`[${delimiters.join("|")}]`); // make a regex to split the string based on the delimiter options
+
+      console.log(delimiter);
+
+      // Split the input string into numbers
+      const cleanedInput = this.inputValue.replace(/\/\/([^\]]+)\\n/g, '');
+
+
+      // Split the this.inputValue into numbers
+      const numbers = cleanedInput.split(delimiterRegex);
+
+      const parsedNumbers = numbers.map(Number);
+      // filter out numbers less that 1000
+      const validNumbers = parsedNumbers.filter(num => Number(num) <= 1000);
+
+      // filter out negative functions and throw an error.
+      const negativeNumbers = validNumbers.filter(num => Number(num) < 0);
+      if (negativeNumbers.length > 0) {
+        const negativeNumbersString = negativeNumbers.join(', ');
+        throw new Error(`Negatives not allowed: ${negativeNumbersString}`);
+      }
+      // Add the numbers together
+      const sum = validNumbers.reduce((acc, curr) => acc + curr, 0);
+      this.sum = sum;
+
+      return sum;
+    }
   }
 }
